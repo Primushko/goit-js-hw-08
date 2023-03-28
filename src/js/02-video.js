e4796fdec2aa89c1dbd7aa93fa086b0a25fbf446
Prymushko Oleh <oleg.prim70p@gmail.com>
2023-03-28 22:55:18 +0300

    ДЗ 8 завдання 8.2. виконано

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const player = new Player('vimeo-player');
+
+player.on(
+    'timeupdate',
+    throttle(function (data) {
+    const currentTime = data.seconds;
+    localStorage.setItem('videoplayer-current-time', currentTime);
+    }, 1000)
+);
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+if (savedTime !== null) {
+    player.setCurrentTime(savedTime);
+}
+
+
 
