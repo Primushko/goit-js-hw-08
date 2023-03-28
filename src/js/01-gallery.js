ac4125cc411765ba7d29dfa0d73cecf39c26f57c
Prymushko Oleh <oleg.prim70p@gmail.com>
2023-03-28 18:14:55 +0300

    28/03/23 18/14

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,5 +3,30 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-const lightbox = new SimpleLightbox('.gallery a');
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const galleryItemsMarkup = galleryItems.reduce(
+    (acc, { preview, original, description }) =>
+    acc +
+    `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">          
+            <img class="gallery__image"
+            src="${preview}"            
+            alt="${description}"/>     
+            </a>
+    </li>`, ``
+);
+gallery.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+gallery.addEventListener(`click`, onGalleryClick);
+function onGalleryClick(event) {
+    event.preventDefault();
+    const target = event.target;
+    if (target.nodeName !== `IMG`) {
+        return;
+    }
+
+    let lightbox = new SimpleLightbox('.gallery a', {
+        captionsData: "alt",
+        captionDelay: 250,
+    });
+}
+// console.log(galleryItems);
