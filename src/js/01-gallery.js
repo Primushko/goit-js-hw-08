474d5da1226f143ea2b30449ed6e52055942e208
Prymushko Oleh <oleg.prim70p@gmail.com>
2023-03-28 17:56:05 +0300

    добавлені скріпти в html

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,7 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+const lightbox = new SimpleLightbox('.gallery a');
 console.log(galleryItems);
